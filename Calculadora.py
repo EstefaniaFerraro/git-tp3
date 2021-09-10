3f03bb9b90cb695d55f734a496f6386bc2fcac7e
EstefaniaFerraro <estefania.ferrro@gmail.com>
2021-09-09 22:49:15 -0300

    Segundo commit, se agregó parte del código del menu

---
+++ Calculadora.py
@@ -0,0 +1,14 @@
+n1 = float(input("Ingresar el primer número: ") )
+n2 = float(input("Ingresar el segundo número: ") )
+
+opcion = 0
+while True:
+    print("""
+    
+    1) Sumar 
+    2) Restar 
+    3) Multiplicar 
+    4) Cambiar los números elegidos
+    5) Apagar calculadora
+    """)
+    opcion = int(input("Elige una opción: ") ) 
