6186cd6f3409443b1f798af7d4e1b61eaf7217a2
EstefaniaFerraro <estefania.ferrro@gmail.com>
2021-09-09 22:50:28 -0300

    Tercer commit, se finalizó el código

--- Calculadora.py
+++ Calculadora.py
@@ -11,4 +11,21 @@ while True:
     4) Cambiar los números elegidos
     5) Apagar calculadora
     """)
-    opcion = int(input("Elige una opción: ") ) 
+    opcion = int(input("Elige una opción: ") )     
+
+    if opcion == 1:
+        print(" ")
+        print("RESULTADO: La suma de",n1,"+",n2,"es igual a",n1+n2)
+    elif opcion == 2:
+        print(" ")
+        print("RESULTADO: La resta de",n1,"-",n2,"es igual a",n1-n2)
+    elif opcion == 3:
+        print(" ")
+        print("RESULTADO: El producto de",n1,"*",n2,"es igual a",n1*n2)
+    elif opcion == 4:
+        n1 = float(input("Ingresa el primer número: ") )
+        n2 = float(input("Ingresa el segundo número: ") )
+    elif opcion == 5:
+        break
+    else:
+        print("La opción ingresada es incorrecta")
